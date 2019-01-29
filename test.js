eafda35d1beff449e1a63d7140c7744d636979df
Emily Marigold Klassen <forivall@gmail.com>
2019-01-29 12:53:49 -0800

    test: improve test logic

--- test.js
+++ test.js
@@ -1,27 +1,22 @@
+import Promise from 'any-promise'
 import test from 'ava'
 
 import generatorToAsync from '.'
 
 test('it works', async (t) => {
-  t.is(
-    await generatorToAsync(function* passthrough(v) {
-      const ret = yield v
-      return ret
-    })(42),
-    42,
-  )
+  const fn = generatorToAsync(function* passthrough(v) {
+    const ret = yield v
+    return ret
+  })
+  t.is(await fn(Promise.resolve(42)), 42)
 })
 
 test('resolve works', async (t) => {
-  t.is(
-    await generatorToAsync.resolve(
-      (function* passthrough(v) {
-        const ret = yield v
-        return ret
-      })(42),
-    ),
-    42,
-  )
+  const iterator = (function* passthrough(v) {
+    const ret = yield v
+    return ret
+  })(42)
+  t.is(await generatorToAsync.resolve(iterator), 42)
 })
 
 test('it catches errors', async (t) => {
