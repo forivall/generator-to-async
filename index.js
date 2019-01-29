d013433b262974e962117f5714e6ec3ac44413c2
Emily Marigold Klassen <forivall@gmail.com>
2019-01-29 12:30:00 -0800

    style: remove semicolons

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
-var Promise = require('any-promise');
+var Promise = require('any-promise')
 
-module.exports = generatorToAsync;
-module.exports.resolve = resolveGenerator;
+module.exports = generatorToAsync
+module.exports.resolve = resolveGenerator
 
 function generatorToAsync(fn) {
   return function() {
-    return resolveGenerator(fn.apply(this, arguments));
-  };
+    return resolveGenerator(fn.apply(this, arguments))
+  }
 }
 
 function resolveGenerator(gen) {
   return new Promise(function(resolve, reject) {
     function step(key, arg) {
-      var info;
-      var value;
+      var info
+      var value
       try {
-        info = gen[key](arg);
-        value = info.value;
+        info = gen[key](arg)
+        value = info.value
       } catch (error) {
-        reject(error);
-        return;
+        reject(error)
+        return
       }
       if (info.done) {
-        resolve(value);
+        resolve(value)
       } else {
         return Promise.resolve(value).then(
           function(value) {
-            step('next', value);
+            step('next', value)
           },
           function(err) {
-            step('throw', err);
+            step('throw', err)
           },
-        );
+        )
       }
     }
-    return step('next');
-  });
+    return step('next')
+  })
 }
